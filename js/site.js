5ea958577275be291c9f6c45da89148c96467ef1
sams91 <103255663+sams91@users.noreply.github.com>
2022-05-30 10:52:01 -0500

    Finished the app and added an about page.

--- js/site.js
+++ js/site.js
@@ -1,5 +1,54 @@
+//get our user input
+function getValue(){
 
-// call hello world
-function helloWorld(){
-    alert("hello World");
+    //make sure the alert is invisible
+    document.getElementById("alert").classList.add("invisible");
+
+    //get user string from the page
+    let userString = document.getElementById("userString").value;
+
+    //check for a palindrome
+    let returnObj = checkForPalindrome(userString);
+
+    //display message to the screen
+    displayMessage(returnObj);
+}
+
+//check if string is a palindrome
+function checkForPalindrome(userString) {
+    
+    //convert everything to lowercase
+    userString = userString.toLowerCase();
+
+    //remove spaces and special characters
+    let regex = /[^a-z0-9]/gi;
+    userString = userString.replace(regex, "");
+
+    let revString = [];
+    let returnObj = {};
+
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];
+        
+    }
+    if (revString == userString) {
+        returnObj.msg = "Well done! You entered a palindrome!"
+    }
+    else{
+        returnObj.msg = "Sorry, you did not enter a palindrome."
+    }
+
+
+    returnObj.reversed = revString;
+
+    return returnObj;
+}
+
+
+//display a message to the string
+function displayMessage(returnObj) {
+
+    document.getElementById("alertHeader").innerHTML = returnObj.msg;
+    document.getElementById("msg").innerHTML = `Your phrase reversed is: ${returnObj.reversed} `;
+    document.getElementById("alert").classList.remove("invisible");
 }
